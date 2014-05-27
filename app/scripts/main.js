85927f3622dd298197783a566b0454e80be5c57f
Justin Petersen <justin@justinpetersen.com>
2014-05-26 19:40:01 -0700

    Integrated Tabletop.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,22 +2,36 @@
 'use strict';
 
 require.config({
-    shim: {
-        bootstrap: {
-            deps: ['jquery'],
-            exports: 'jquery'
-        }
-    },
     paths: {
         jquery: '../bower_components/jquery/dist/jquery',
         backbone: '../bower_components/backbone/backbone',
         underscore: '../bower_components/underscore/underscore',
-        bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap'
+        bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap',
+        tabletop: '../bower_components/tabletop/src/tabletop',
+        'backbone.tabletopSync': '../bower_components/tabletop/src/backbone.tabletopSync',
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery'],
+            exports: 'jquery'
+        },
+        tabletop: {
+            deps: ['jquery'],
+            exports: 'Tabletop'
+        },
+        'backbone.tabletopSync': {
+            deps: ['backbone'],
+            exports: 'Backbone'
+        },
     }
 });
 
 require([
-    'backbone'
-], function (Backbone) {
+    'tabletop',
+    'backbone.tabletopSync'
+], function (Tabletop, Backbone) {
+    Tabletop.init( { key: '0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE',
+                   callback: function(data, tabletop) { console.log(data) },
+                   simpleSheet: true } );
     Backbone.history.start();
 });
