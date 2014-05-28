c3c2c3839f4813a08b372e746fa97d4a6bdd567d
Justin Petersen <justin@justinpetersen.com>
2014-05-28 12:20:55 -0700

    Added Tabletop.js back in

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,72 +7,64 @@ require.config({
         underscore: '../bower_components/underscore/underscore',
         backbone: '../bower_components/backbone/backbone',
         'backbone.localStorage': '../bower_components/backbone.localStorage/backbone.localStorage',
+        'backbone.tabletopSync': '../scripts/helpers/backbone.tabletopSync',
+        tabletop: '../bower_components/tabletop/src/tabletop',
         bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap'
     },
   shim: {
     underscore: {
       exports: "_"
     },
-    backbone: {
-      deps: ['underscore', 'jquery'],
-      exports: 'Backbone'
+    tabletop: {
+        deps: [],
+        exports: 'Tabletop'
     }
     // bootstrap: {
     //     deps: ['jquery'],
     //     exports: 'jquery'
     // }
-    // tabletop: {
-    //     deps: [],
-    //     exports: 'Tabletop'
-    // }
   }
 });
 
-// require([
-//     'tabletop',
-//     'backbone.tabletopSync'
-// ], function (Tabletop, Backbone) {
-//     console.log('Tabletop: ' + Tabletop);
-//     console.log('Backbone: ' + Backbone);
-//     console.log('Backbone.tabletopSync' + Backbone.tabletopSync);
-//     Tabletop.init( { key: '0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE',
-//                    callback: function(data, tabletop) { console.log(data) },
-//                    simpleSheet: true } );
-//     Backbone.history.start();
-
-// });
-
 require([
-    'jquery',
-    'backbone',
-    'models/Todo',
-    'views/MasterView'
-  ], function($, Backbone, Todo, MasterView ) {
+        'jquery',
+        'tabletop',
+        'backbone.tabletopSync',
+        'models/Todo',
+        'views/MasterView'
+    ], function($, Tabletop, Backbone, Todo, MasterView ) {
 
-  var Router = Backbone.Router.extend({
-    routes: {
-      "": "main"
-    },
+    // console.log('Tabletop: ' + Tabletop);
+    // console.log('Backbone: ' + Backbone);
+    // console.log('Backbone.tabletopSync' + Backbone.tabletopSync);
+    Tabletop.init( { key: '0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE',
+                   callback: function(data, tabletop) { console.log(data) },
+                   simpleSheet: true } );
 
-    main: function(){
-      var tasks = new Todo.Collection();
-      var view = new MasterView({collection: tasks});
-      tasks.fetch({
-        success: function(tasks){
-          $("#container").html(view.render().el).show();
+    var Router = Backbone.Router.extend({
+            routes: {
+            "": "main"
         },
-        error: function(model, error) {
-          // TODO: handle errors nicer
-          alert(error);
+
+        main: function(){
+            var tasks = new Todo.Collection();
+            var view = new MasterView({collection: tasks});
+            tasks.fetch({
+                success: function(tasks){
+                    $("#container").html(view.render().el).show();
+                },
+                error: function(model, error) {
+                    // TODO: handle errors nicer
+                    alert(error);
+                }
+            });
         }
-      });
-    }
-  });
+    });
 
-  // Preload CSS Sprite
-  $('<img/>').attr('src', "./styles/glyphicons.png");
+    // Preload CSS Sprite
+    $('<img/>').attr('src', "./styles/glyphicons.png");
 
-  var router = new Router();
-  Backbone.history.start();
+    var router = new Router();
+    Backbone.history.start();
  
 });
