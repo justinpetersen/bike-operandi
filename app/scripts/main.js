860cc186f28b9373c996ee346099c8ec811f9a48
Justin Petersen <justin@justinpetersen.com>
2014-06-04 11:01:10 -0700

    Converted data sync from Tabletop.js to Firebase

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,88 +6,60 @@ require.config({
         jquery: '../bower_components/jquery/dist/jquery',
         underscore: '../bower_components/underscore/underscore',
         backbone: '../bower_components/backbone/backbone',
-        'backbone.localStorage': '../bower_components/backbone.localStorage/backbone.localStorage',
-        'backbone.tabletopSync': '../scripts/helpers/backbone.tabletopSync',
-        tabletop: '../bower_components/tabletop/src/tabletop',
+        // 'backbone.localStorage': '../bower_components/backbone.localStorage/backbone.localStorage',
         bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap',
-        'jquery.carousel.fullscreen': '../scripts/helpers/jquery.carousel.fullscreen'
+        'jquery.carousel.fullscreen': '../scripts/helpers/jquery.carousel.fullscreen',
+        firebase: '../bower_components/firebase/firebase',
+        'firebase-simple-login': '../bower_components/firebase-simple-login/firebase-simple-login',
+        'backfire': '../bower_components/backfire/backbone-firebase',
     },
   shim: {
     underscore: {
       exports: "_"
     },
-    tabletop: {
-        deps: [],
-        exports: 'Tabletop'
+    backbone: {
+        deps: ['underscore', 'jquery'],
+        exports: 'Backbone'
     },
     bootstrap: {
         deps: ['jquery'],
         exports: 'jquery'
+    },
+    'firebase-simple-login': {
+        deps: ['firebase'],
+        exports: 'Firebase'
+    },
+    backfire: {
+        deps: ['backbone', 'firebase'],
+        exports: 'Firebase'
     }
-
   }
 });
 
 require([
         'jquery.carousel.fullscreen',
-        'tabletop',
-        'backbone.tabletopSync',
-        'models/Todo',
-        'views/MasterView'
-    ], function($, Tabletop, Backbone, Todo, MasterView) {
+        'backbone',
+        'backfire',
+        'firebase-simple-login'
+    ], function($, Backbone, Firebase) {
 
-    // return;
-
-    // console.log('Tabletop: ' + Tabletop);
+    // console.log('$: ' + $);
     // console.log('Backbone: ' + Backbone);
-    // console.log('Backbone.tabletopSync' + Backbone.tabletopSync);
-    // Tabletop.init({key: '0AmYzu_s7QHsmdE5OcDE1SENpT1g2R2JEX2tnZ3ZIWHc',
-    //         callback: function(data, tabletop) {
-    //             console.log(data);
-    //         },
-    //         simpleSheet: true});
+    // console.log('Firebase: ' + Firebase);
 
-    var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1qfVHafHl01zYduxnwdHdjL1HN_3ljSF2UU5bZyJ0PWY/pubhtml';
-
-    /* 
-    You need to declare the tabletop instance separately, then feed it into the model/collection
-    You *must* specify wait: true so that it doesn't try to fetch when you initialize
-    */
-    var storage = Tabletop.init( { key: public_spreadsheet_url,
-                                  wait: true } );
-
-    /*
-    Need to specify that you'd like to sync using Backbone.tabletopSync
-    Can specify tabletop attributes, or you can do it on the collection
-    */
-    var Cat = Backbone.Model.extend({
-        idAttribute: 'name',
+    var Bike = Backbone.Model.extend({
         defaults: {
             title: '',
             image: ''
-        },
-        tabletop: {
-            instance: storage,
-            sheet: 'bikes'
-        },
-        sync: Backbone.tabletopSync
+        }
+    })
+
+    var BikeCollection = Backbone.Firebase.Collection.extend({
+      model: Bike,
+      firebase: new Firebase("https://bike-operandi.firebaseio.com/bikes")
     });
 
-    /*
-    Need to specify that you'd like to sync using Backbone.tabletopSync
-    Need to specify a tabletop key and sheet
-    */
-    var CatCollection = Backbone.Collection.extend({
-        // Reference to this collection's model.
-        model: Cat,
-        tabletop: {
-            instance: storage,
-            sheet: 'bikes'
-        },
-        sync: Backbone.tabletopSync
-    });
-
-    var CatView = Backbone.View.extend({
+    var BikeView = Backbone.View.extend({
         tagname: 'div',
         attributes: {class: 'item'},
         template: _.template($('#item-template').html()),
@@ -98,20 +70,29 @@ require([
         }
     });
 
-    var cats = new CatCollection();
-    cats.fetch({ success: showInfo });
+    var AppView = Backbone.View.extend({
+        bikes: [],
 
-    function showInfo(cats) {
-        for (var i=0; i<cats.length; i++) {
-            var cat = new CatView({ model: cats.at(i) });
-            var item = $('#container').append(cat.render().el);
+        initialize: function(bikes) {
+            this.bikes = bikes;
+            this.listenTo(this.bikes, 'all', this.render);
+        },
+
+        render: function() {
+            for (var i=0; i<this.bikes.length; i++) {
+                var bike = new BikeView({ model: this.bikes.at(i) });
+                var item = $('#container').append(bike.render().el);
+            }
+
+            $('#container').children().first().addClass('active');
+
+            // TODO: Improve the structure of bootstrap.carousel.fullscreen
+            $.carouselFullscreen();
+            // $('.carousel').carousel();
         }
-        console.log($('#container').children().first().addClass);
-        $('#container').children().first().addClass('active');
+    });
 
-        // TODO: Improve the structure of bootstrap.carousel.fullscreen
-        $.carouselFullscreen();
-        // $('.carousel').carousel();
-    }
+    var bikes = new BikeCollection();
+    var appView = new AppView(bikes);
  
 });
