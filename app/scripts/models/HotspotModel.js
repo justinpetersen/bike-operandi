c6142d8efcf231041906d6a57769649157b0feed
Justin Petersen <justin@justinpetersen.com>
2014-06-30 17:05:16 -0700

    Added Amazon Affiliates tag to product URL

--- app/scripts/models/HotspotModel.js
+++ app/scripts/models/HotspotModel.js
@@ -14,13 +14,18 @@ define([
             url: '#'
         },
 
-        initialize: function() {
+        onSync: function() {
             if (this.get('url') == this.defaults.url) {
-                this.set('url', 'http://www.amazon.com/exec/obidos/ASIN/' + this.get('asin'));
+                this.set('url', 'http://www.amazon.com/exec/obidos/ASIN/' + this.get('asin') + '?tag=bikeoper-20');
+                console.log('url: ' + this.get('url'));
             }
             if (this.get('image') == this.defaults.image) {
                 this.set('image', 'http://images.amazon.com/images/P/' + this.get('asin') + '.01.MZZZZZZZ.jpg');
             }
+        },
+
+        initialize: function() {
+            this.listenTo(this, 'sync', this.onSync);
         }
     });
 
